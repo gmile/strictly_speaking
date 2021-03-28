aae25eb087c85e97f871c90fd8822c615c1c50e4
Eugene Pirogov <iamexile@gmail.com>
2021-03-28 22:44:13 +0300

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StrictlySpeaking.MixProject do
   def project do
     [
       app: :strictly_speaking,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
